153ea2c9bb70156d8746c54d5d5c91ca8332388a
Adarsh Dhananjaya k <adarshsurekha@gmail.com>
2024-10-26 03:43:30 +0530

    updated output path

--- melo_tts/melo_tts.py
+++ melo_tts/melo_tts.py
@@ -13,5 +13,5 @@ model = TTS(language="EN", device=device)
 speaker_ids = model.hps.data.spk2id
 
 text = "In the heart of the bustling city, where the sounds of traffic blend with the laughter of children playing in the park, a small café stands quietly, inviting passersby with the rich aroma of freshly brewed coffee and baked pastries; it is a place where stories are shared, friendships are formed, and time seems to slow down just enough for one to savor the simple pleasures of life"
-output_path = ".gcpu_text_en-us.wav"
+output_path = "melo_tts_output.wav"
 model.tts_to_file(text, speaker_ids["EN-Default"], output_path, speed=speed)
