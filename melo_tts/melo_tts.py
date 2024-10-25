6c799ef120f59c1ca0bdac813d83b9e28f846fd9
Adarsh Dhananjaya k <adarshsurekha@gmail.com>
2024-10-26 03:39:15 +0530

    update dockerfile

--- melo_tts/melo_tts.py
+++ melo_tts/melo_tts.py
@@ -3,7 +3,7 @@ from melo.api import TTS
 
 # CPU is sufficient for real-time inference.
 # You can set it manually to 'cpu' or 'cuda' or 'cuda:0' or 'mps'
-device = "auto"  # Will automatically use GPU if available
+device = "cuda:0"  # Will automatically use GPU if available
 speed = 1.0
 
 # English
@@ -13,5 +13,5 @@ model = TTS(language="EN", device=device)
 speaker_ids = model.hps.data.spk2id
 
 text = "In the heart of the bustling city, where the sounds of traffic blend with the laughter of children playing in the park, a small café stands quietly, inviting passersby with the rich aroma of freshly brewed coffee and baked pastries; it is a place where stories are shared, friendships are formed, and time seems to slow down just enough for one to savor the simple pleasures of life"
-output_path = "./cpu_text_en-us.wav"
+output_path = ".gcpu_text_en-us.wav"
 model.tts_to_file(text, speaker_ids["EN-Default"], output_path, speed=speed)
